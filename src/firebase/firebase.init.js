2ce754eb46109d83e2d33ac430f42c2d6e91dd6f
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-08-15 22:52:47 +0600

    environment varibales are in init file

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjS_sQr0JzQFE0_romlCi3slC9oI7ybNM",
-  authDomain: "crespo-recipes.firebaseapp.com",
-  projectId: "crespo-recipes",
-  storageBucket: "crespo-recipes.appspot.com",
-  messagingSenderId: "123862563289",
-  appId: "1:123862563289:web:65fb41ca846102388781c8",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 export const app = initializeApp(firebaseConfig);
